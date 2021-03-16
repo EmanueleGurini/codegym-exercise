7b6460f293d4018a681b3939da33679d4a60ece0
Emanuele Gurini <emanuelegurini@Air-di-Emanuele.fritz.box>
2021-03-16 11:13:46 +0100

    Task 1104

---
+++ src/inheritance/Task1104.java
@@ -0,0 +1,27 @@
+package inheritance;
+
+/*
+Write the following four classes: Employee, Manager, CEO, and Secretary.
+Make the Manager, CEO and Secretary classes inherit the Employee class.
+ */
+
+public class Task1104 {
+    public static void main(String[] args) {
+    }
+
+    public class Employee{
+
+    }
+
+    public class Manager extends Employee {
+
+   }
+
+    public class CEO extends Employee {
+
+    }
+
+    public class Secretary extends Employee {
+
+    }
+}
