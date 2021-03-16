aa56e1dfabee8a6ff28141fb947ff1556c199a55
Emanuele Gurini <emanuelegurini@Air-di-Emanuele.fritz.box>
2021-03-16 10:43:38 +0100

    Task 1102 to 1103

---
+++ src/inheritance/Task1102.java
@@ -0,0 +1,18 @@
+package inheritance;
+
+public class Task1102 {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+}
