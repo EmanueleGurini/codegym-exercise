0a3a9a6273af0f23c9c04f074bbafe0cd1c1e759
Emanuele Gurini <emanuele.gurini89@gmail.com>
2021-03-23 16:53:09 +0100

    Polymorphism

---
+++ src/polymorphism/Task1201.java
@@ -0,0 +1,24 @@
+package polymorphism;
+
+/**
+ * Override the getName method in the Whale class, so that the program displays:
+ * I'm not a cow. I'm a whale.
+ */
+
+public class Task1201 {
+    public static void main(String[] args) {
+        Cow cow = new Whale();
+
+        System.out.println(cow.getName());
+    }
+
+    public static class Cow {
+        public String getName() {
+            return "I'm a cow";
+        }
+    }
+
+    public static class Whale extends Cow {
+
+    }
+}
